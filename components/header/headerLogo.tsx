930dc4c2d71d674de3193398ff50be50acc2b7e4
mangareira <ghabryellsantos81@gmail.com>
2025-03-18 15:40:47 -0300

    rename-logo

--- components/header/headerLogo.tsx
+++ components/header/headerLogo.tsx
@@ -12,7 +12,7 @@ export const HeaderLogo = () => {
           src={'/logo.svg'}
           width={28}
         />
-        <p className="font-semibold text-white text-2xl ml-2.5">Finaceiro</p>
+        <p className="font-semibold text-white text-2xl ml-2.5">Financeiro</p>
       </div>
     </Link>
   );
